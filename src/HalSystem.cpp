d7d995e0cdc3cefd5548020f848ba986ddf77a1f
0x1abin <0x1abin@gmail.com>
2026-08-27 01:57:18 +0800

    fix: mirror device model HAL API

--- src/HalSystem.cpp
+++ src/HalSystem.cpp
@@ -1,6 +1,7 @@
 #include "HalSystem.h"
 
 #include <Arduino.h>
+#include <BoardConfig.h>
 
 void HalSystem::begin() {}
 void HalSystem::restart() { exit(0); }
@@ -8,6 +9,7 @@ void HalSystem::checkPanic() {}
 void HalSystem::clearPanic() {}
 std::string HalSystem::getPanicInfo(bool full) { return {}; }
 bool HalSystem::isRebootFromPanic() { return false; }
+const char* HalSystem::getDeviceModel() { return BoardConfig::ACTIVE.name; }
 bool HalSystem::getDeviceId(DeviceId& out) {
   out = {0x02, 0x00, 0x00, 0x00, 0x00, 0x01};
   return true;
